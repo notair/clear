0a846c97ece4162dc668a81d047ccc915cef4d3d
zQi-PC\zQi <318319903@qq.com>
2016-10-26 12:35:04 +0800

    no message

--- index.php
+++ index.php
@@ -48,8 +48,80 @@
 	</header>
 
 	<article>
-	  <h1>Internet Explorer 9</h1>
-	  <p> Windows Internet Explorer 9(缩写为 IE9 )在2011年3月14日21:00 发布。</p>
+<div id="blog" class="site-content">
+	<div class="random">
+	<?php
+	global $post;
+	$postid = $post->ID;
+	$args = array( ‘orderby’ => ‘rand’, ‘post__not_in’ => array($post->ID), ‘showposts’ =>2);
+	//query_posts("orderby=rand&order=asc&limit=1&showposts=3")
+	$query_posts = new WP_Query();
+	//$query_posts->query($args);
+	$args=$query_posts->query("orderby=rand&order=asc&limit=1&showposts=3");
+	?>
+	<?php while ($query_posts->have_posts()) : $query_posts->the_post(); ?>
+	<div class="random-img"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_blank"><img src="<?php echo catch_first_image();?>" ></a></div>
+	<?php endwhile; ?>
+	</div>
+
+			<?php  
+				if(have_posts()) :
+			?>
+			<ul id="timeline" class="clearfix">
+				<li>
+					<article <?php post_class( 'clearfix' ); ?>>
+						<header class="entry-header">
+							<!-- END .entry-meta 
+							<?php if(has_post_thumbnail()) : ?>
+							<div class="entry-featured">
+								<figure class="entry-thumnail">
+									<?php the_post_thumbnail( 'post-thumb' ); ?>
+								</figure>
+							</div>			
+							<?php endif; ?>
+							END .entry-featured -->
+
+							<h1 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" target="_blank"><?php the_title(); ?></a></h1>
+
+							<div class="entry-meta">
+								<span class="entry-date">
+									<?php echo twenty_posted_on(); ?>
+									<?php if(function_exists('the_views')) { the_views(); } ?>
+								</span>
+							</div>
+
+						</header>
+
+				<div id="preview">
+				<?php if (get_first_image()) {?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_blank"><img src="<?php echo get_first_image(); ?>" alt="<?php the_title(); ?>" ></a>
+				<?php } ?>
+				</div>
+
+						<!-- END .entry-header -->
+						<div class="entry-summary">	<?php the_excerpt(); ?>
+							<a href="<?php the_permalink();; ?>" textalign="right" target="_blank"  style="font-weight: 700;">>>>查看全文</a>
+						</div>
+
+						<!-- END .entry-summanry -->
+					</article>
+				</li>
+			</ul>
+			<div class="infinite-loader"></div>
+			<div class="pagination clearfix">
+				<?php twenty_paging_nav(); ?>
+			</div>
+			<?php else: 
+					get_template_part( 'content', 'none' );
+				endif;
+			?>
+		</main>
+		<!-- END site-main -->
+	</div>
+	<!-- END #primary -->
+
+</div>
+<!-- END .site-content -->
 	</article>
 
 	<aside>
